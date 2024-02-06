441ddbf9b11d3ae635e4ce769235ba95a36a58c6
Drew Bogdan <drew@bnimble.org>
2024-02-06 14:25:22 -0800

    Attempt at dataframe

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.title("Hello World")
+testData = {
+            "Instance 1": ["1", "10/18/2022", "Linux", "ssh"],
+            "Instance 2": ["5", "10/18/2022", "Linux", "ufw"],
+            "Instance 3": ["2", "10/18/2022", "Windows", "Chrome"],
+            "Instance 4": ["3", "10/18/2022", "iOS", "Finder"],
+            "Instance 5": ["1", "10/18/2022", "Windows", "Microsoft Word"],
+            "Instance 6": ["2", "10/18/2022", "iOS", "Messaging"],
+            "Instance 7": ["3", "10/18/2022", "Linux", "fail2ban"],
+           }
+
+st.title("Auto-CTI
+")
+
+st.dataframe(data=testData, columns=("Threat Level", "Date Found", "Compromized OS", "Compromised App"))

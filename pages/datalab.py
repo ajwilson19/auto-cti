4b65f9f22d657c5440fa81b06f46c179f8860a6a
AJ Wilson <awilson22@zagmail.gonzaga.edu>
2024-02-18 16:01:42 -0800

    estimate, gitignore

--- pages/datalab.py
+++ pages/datalab.py
@@ -2,13 +2,9 @@ import streamlit as st
 import requests
 
 st.title("Chalice x GPT")
-st.write("See guide [here](https://cookbook.openai.com/examples/how_to_format_inputs_to_chatgpt_models)")
-st.write("Link guide [here](https://community.openai.com/t/how-is-gpt-4-getting-this-information-from-a-url/529074)")
-
-# if 'messages' not in st.session_state:
-#     st.session_state.messages = []
-# else:
-#     messages = st.session_state.messages
+# with st.expander("Resources"):
+#     st.write("See guide [here](https://cookbook.openai.com/examples/how_to_format_inputs_to_chatgpt_models)")
+#     st.write("Link guide [here](https://community.openai.com/t/how-is-gpt-4-getting-this-information-from-a-url/529074)")
 
 messages = [{
                 "role": "system",
@@ -16,19 +12,8 @@ messages = [{
             },
             {"role": 'user', "content":""}]
 
-#role = st.selectbox(label="Select Role:", options=["system", "user", "assistant"], index=1)
 link = st.text_input(label="Link:", value="")
 messages[1]['content'] = f'Here is a URL: {link}'
-
-#st.write(messages)
-
-# if st.button(label="Reset"):
-#     st.session_state.messages = []
-
-# if st.session_state.messages == []:
-#     st.warning("Please enter a message before requesting response")
-# else:
-    
 
 st.code(
     """response = client.chat.completions.create(
@@ -45,6 +30,12 @@ if st.button("Generate"):
         response = requests.post(url, json=request).json()
 
     st.write(response['choices'][0]['message']['content'])
-    st.write(response['usage'])
+    usage = response['usage']
+    in_tokens = usage['prompt_tokens']
+    out_tokens = usage['completion_tokens']
+
+    estimate = (in_tokens / 1000) * 0.03 + (out_tokens / 1000) * 0.06
+    st.warning(f'Estimated cost: ${estimate}')
+
 
     

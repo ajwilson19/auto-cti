c57347a36a4c58d8e0e89a122b7413dbf8d03be8
AJ Wilson <awilson22@zagmail.gonzaga.edu>
2024-03-06 12:29:21 -0800

    hash pass and create users

--- login.py
+++ login.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from pymongo import MongoClient
+import bcrypt
+
 def sidebar():
     client = MongoClient(st.secrets['uri'])
     db = client['test']
@@ -14,14 +16,19 @@ def sidebar():
             username = st.text_input("Username:")
             password = st.text_input("Password:")
             if st.button("Login"):
-                # add hash
-                if auth.find_one({"user": username, "pass": password}):
-                    st.session_state['user'] = username
-                    st.rerun()
+                user = auth.find_one({"user": username})
+                if user:
+                    if bcrypt.checkpw(password.encode('utf-8'), user["pass"]):
+                        st.session_state['user'] = username
+                        st.rerun()
+                    else:
+                        st.error("Incorrect password")
                 else:
-                    st.error("Incorrect username/password")
+                    st.error("User does not exist")
         else:
             st.title(f"Hello {st.session_state['user']}")
             if st.button("Logout"):
                 st.session_state['user'] = None
                 st.rerun()
+
+    return auth
--- pages/profile.py
+++ pages/profile.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import bcrypt
 import login
-login.sidebar()
+import time
+
+auth = login.sidebar()
 
 if st.session_state['user']:
     st.title(st.session_state['user'])
@@ -8,3 +11,22 @@ if st.session_state['user']:
     #st.selectbox()
 else:
     st.warning("Login or create an account")
+
+    with st.expander("Create an account"):
+        username = st.text_input("Username")
+        password = st.text_input('Password')
+        #try:
+        if auth.find_one({"user": username}):
+            st.warning("User already exists")
+        elif str(username) and str(password):
+            hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            if st.button("Create User"):
+                with st.spinner("Creating User"):
+                    auth.insert_one({"user": username, "pass": hashed})
+                    time.sleep(2)
+                st.session_state['user'] = username
+                st.rerun()
+            
+
+        # except:
+        #      st.error("Invalid username and password")

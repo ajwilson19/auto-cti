5f2bbe3d94e176960737905beb3c5e97611efe05
AJ Wilson <awilson22@zagmail.gonzaga.edu>
2024-05-02 15:46:52 -0700

    rm datetime, get feed, profile->settings

--- auto-cti/chalicelib/WebScrape.py
+++ auto-cti/chalicelib/WebScrape.py
@@ -103,7 +103,7 @@ def get_formatted_articles(links) -> 'Formatted Article Link':
         try:
             formatted.append([link, scrape_article(link)])
         except:
-            pass
+            print("Error on Link:",link)
     return formatted
 
 """
